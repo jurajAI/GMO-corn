e0b854166f8f93d30b794d6b6db7f3754153eebe
jurajAI <154024397+jurajAI@users.noreply.github.com>
2023-12-18 08:27:02 +0100

    Update configs.py

--- yolov3/configs.py
+++ yolov3/configs.py
@@ -43,7 +43,7 @@ TRAIN_LOGDIR                = "log"
 TRAIN_CHECKPOINTS_FOLDER    = "checkpoints"
 TRAIN_MODEL_NAME            = f"{YOLO_TYPE}_custom"
 TRAIN_LOAD_IMAGES_TO_RAM    = True # With True faster training, but need more RAM
-TRAIN_BATCH_SIZE            = 1 # 4 !!!
+TRAIN_BATCH_SIZE            = 4 
 TRAIN_INPUT_SIZE            = 608 # 416 !!!
 TRAIN_DATA_AUG              = True
 TRAIN_TRANSFER              = True
@@ -55,7 +55,7 @@ TRAIN_EPOCHS                = 100
 
 # TEST options
 TEST_ANNOT_PATH             = "yolo/yolo_annotations_test.txt" # "carpk_data/yolo_annotations_test.txt" !!!
-TEST_BATCH_SIZE             = 1 # 4 !!!
+TEST_BATCH_SIZE             =  4 
 TEST_INPUT_SIZE             = 608 # 416 !!!
 TEST_DATA_AUG               = False
 TEST_DECTECTED_IMAGE_PATH   = ""
